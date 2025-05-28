ed43addd116f7844b9892385d0d0105ff7d6a473
oneon360 <portfalcon@gmail.com>
2025-05-29 07:57:54 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+function hexToBase64($hex) {
+    return base64_encode(hex2bin($hex));
+}
+
 $id = $_GET['id'] ?? null;
 
 if (!$id) {
@@ -23,6 +27,28 @@ if (!isset($keys[$id])) {
     exit;
 }
 
-// Output: key_id:key_value
-header('Content-Type: text/plain');
-echo $keys[$id];
+$raw = explode(':', $keys[$id]);
+
+if (count($raw) !== 2) {
+    http_response_code(500);
+    echo "Invalid key format";
+    exit;
+}
+
+$key_id_hex = $raw[0];
+$key_hex = $raw[1];
+
+$kid_b64 = hexToBase64($key_id_hex);
+$k_b64 = hexToBase64($key_hex);
+
+// Output JSON
+header('Content-Type: application/json');
+echo json_encode([
+    "keys" => [
+        [
+            "kty" => "oct",
+            "kid" => $kid_b64,
+            "k"   => $k_b64
+        ]
+    ]
+]);
